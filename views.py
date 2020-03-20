28f0e2b46b21e5a81ee1cff2fe930873b5c97fbb
Simeon Keske <ca-git@noemis.me>
2020-03-20 16:35:01 +0100

    change to be a django app

---
+++ views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.urls import path
+from . import views
+
+app_name = 'netzkarte'
+urlpatterns = [
+    path('netzkarte', views.gtfsexport, name='netzkarte')
+]
