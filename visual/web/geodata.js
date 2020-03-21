03f2854f9ae6a7f6bb18eed85bfd29f44d5da9fb
Jana-Z <github3@jana-zeller.de>
2020-03-22 00:20:30 +0100

    Line width changes according to time/distance

--- visual/web/geodata.js
+++ visual/web/geodata.js
@@ -11,32 +11,58 @@ function createMap() {
 
     var promises = [
         d3.json("4_niedrig.geojson"),
-        d3.json("https://fahrplandatengarten.de/netzkarte/api")
+        d3.json("https://fahrplandatengarten.de/netzkarte/d3api")
     ];
     Promise.all(promises).then(ready);
 
-    function ready(parm) {
+    function ready(param) {
 
         svg1.append("g")
             .selectAll("path")
-            .data(parm[0]["features"])
+            .data(param[0]["features"])
             .enter()
             .append("path")
             .attr("d", d3.geoPath()
                 .projection(projection))
-            .attr("fill", "red");
+            .attr("fill", "#5b67bf");
+
+        var lineGenerator = d3.line();
+        var pathGroup = svg1.append("g")
+        var paths = pathGroup.selectAll("path")
+            .data(param[1]['connections'])
+            .enter()
+                .append("path")
+                    .attr("stroke", "black")
+                    .attr("fill", "black")
+                    .attr("d", function(d, i){
+                        return lineGenerator(d.link.map(projection))
+                    })
+                    .attr("stroke-width", function(d){
+                      console.log(this.getTotalLength())
+                      return (d.duration / this.getTotalLength()) / 8
+                    })
+                    .attr("opacity", 0.2)
+                    .attr("display", "none")
 
         svg1.append("g")
             .selectAll("circle")
-            .data(Object.values(parm[1]))
+            .data(param[1]['stations'])
             .enter()
-                .append("circle")               
+                .append("circle")
                     .attr("r", 5)
                     .attr("fill", "green")
                     .attr("transform", function(d, i){
-                        return "translate(" + projection([d.lon, d.lat])+")"
+                        return "translate(" + projection(d.location)+")"
                     })
-                    
+                .on("click", function draw_lines(d, i){
+                    paths.filter(path_data => {return JSON.stringify(path_data.link[0]) == JSON.stringify(d.location)})
+                      .attr("display", "block")
+                    paths.filter(path_data => { return JSON.stringify(path_data.link[0]) != JSON.stringify(d.location)})
+                      .attr("display", "none")
+                });
+
+
+
     }
 
 }
