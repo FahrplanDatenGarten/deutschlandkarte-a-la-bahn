b6089f2e138c07bbe4f97bcd41652487a88b5c81
Leo Maroni <me@leomaroni.de>
2020-03-23 11:56:34 +0100

    Added Caching for 2 hours on d3api

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
 
 from . import views
@@ -11,5 +12,5 @@ urlpatterns = [
             template_name="netzkarte/index.html"),
         name='index'),
     path('api', views.convert_toJson, name='api'),
-    path('d3api', views.d3_tree, name='d3api'),
+    path('d3api', cache_page(60 * 120)(views.d3_tree), name='d3api'),
 ]
