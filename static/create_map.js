60d42d3bee5b00ef9bd2978c585cb78bd7a4f122
Leo Maroni <git@em0lar.de>
2020-10-11 10:13:55 +0200

    Formatted JS

--- static/create_map.js
+++ static/create_map.js
@@ -1,24 +1,24 @@
 height = 800;
 width = 700;
 
-var svg1 = d3.select("#net")
+let svg1 = d3.select("#net")
     .append("svg")
     // Responsive SVG needs these 2 attributes and no width and height attr.
     .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox","0 0 " + width + " " + height)
+    .attr("viewBox", "0 0 " + width + " " + height)
     // Class to make it responsive.
     .classed("svg-content-responsive", true)
 
-var projection = d3.geoMercator()
+let projection = d3.geoMercator()
     .scale(3000)
     .center([10, 51])
     .translate([width / 2, height / 2]);
 
-var path = d3.geoPath()
+let path = d3.geoPath()
     .projection(projection);
 
 
-var promises = [
+let promises = [
     d3.json(d3_geojson_url),
     d3.json(d3_api_url)
 ];
@@ -36,19 +36,18 @@ function ready(param) {
             .projection(projection))
         .attr("fill", "#C0E1D7");
 
-    var lineGenerator = d3.line();
-    var pathGroup = svg1.append("g");
-    var paths = pathGroup.selectAll("path")
+    let lineGenerator = d3.line();
+    let pathGroup = svg1.append("g");
+    let paths = pathGroup.selectAll("path")
         .data(param[1]['connections'])
         .enter()
         .append("path")
         .attr("stroke", "#000000")
         .attr("fill", "#000000")
-        .attr("d", function(d, i) {
+        .attr("d", function (d, i) {
             return lineGenerator(d.link.map(projection))
         })
-        .attr("stroke-width", function(d) {
-            // console.log(this.getTotalLength())
+        .attr("stroke-width", function (d) {
             return (d.duration / this.getTotalLength()) / 8
         })
         .attr("opacity", 0.2)
@@ -61,17 +60,17 @@ function ready(param) {
         .append("circle")
         .attr("r", 5)
         .attr("fill", "#EC0016")
-        .attr("transform", function(d, i) {
+        .attr("transform", function (d, i) {
             return "translate(" + projection(d.location) + ")"
         })
         .on("click", function draw_lines(d, i) {
             paths.filter(path_data => {
-                    return JSON.stringify(path_data.link[0]) == JSON.stringify(d.location);
-                })
+                return JSON.stringify(path_data.link[0]) === JSON.stringify(d.location);
+            })
                 .attr("display", "block");
             paths.filter(path_data => {
-                    return JSON.stringify(path_data.link[0]) != JSON.stringify(d.location);
-                })
+                return JSON.stringify(path_data.link[0]) !== JSON.stringify(d.location);
+            })
                 .attr("display", "none");
         })
         .on("mouseover", handleMouseOver)
@@ -81,12 +80,12 @@ function ready(param) {
         // show station names
         svg1.append("text").text(d.name)
             .attr("transform", "translate(" + projection(d.location) + ")")
-    };
+    }
 
 
     function handleMouseOut(d, i) {
         // removing station names
         svg1.selectAll("text").remove()
-    };
+    }
 
 }
